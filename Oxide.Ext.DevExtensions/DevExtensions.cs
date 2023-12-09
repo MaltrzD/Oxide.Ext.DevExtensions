dc9f9a757022b0a37b09666ffdab51999471a59f
MaltrzD <97359122+MaltrzD@users.noreply.github.com>
2023-12-10 01:23:46 +0300

    Update DevExtensions

--- Oxide.Ext.DevExtensions/DevExtensions.cs
+++ Oxide.Ext.DevExtensions/DevExtensions.cs
@@ -1,11 +1,12 @@
 ﻿namespace Oxide.Ext.DevExtensions
 {
-
+    //DevExtension initial class:)
     public static class DevExtensions
     {
         public static string GetMeme()
         {
             string[] memes = new string[] { "карбон крут", "https://youtu.be/dQw4w9WgXcQ?si=ZkgNSrX8D5bR5LeV", "meme" };
+            
 
             return memes.GetRandom();
         }
