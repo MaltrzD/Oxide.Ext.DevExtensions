ee491266e1fe6c414d11adaf5cccc47d50f448d5
MaltrzD <97359122+MaltrzD@users.noreply.github.com>
2023-12-10 01:34:01 +0300

    Updates

--- Oxide.Ext.DevExtensions/DevExtensions.cs
+++ Oxide.Ext.DevExtensions/DevExtensions.cs
@@ -3,6 +3,10 @@
     //DevExtension initial class:)
     public static class DevExtensions
     {
+        /// <summary>
+        /// Создан как точка создания библиотеки
+        /// </summary>
+        /// <returns>строка с мемом</returns>
         public static string GetMeme()
         {
             string[] memes = new string[] { "карбон крут", "https://youtu.be/dQw4w9WgXcQ?si=ZkgNSrX8D5bR5LeV", "meme" };
--- Oxide.Ext.DevExtensions/Extensions/ListExtensions.cs
+++ Oxide.Ext.DevExtensions/Extensions/ListExtensions.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
 
-namespace Oxide.Ext.DevExtensions
+namespace Oxide.Ext.DevExtensions.Extensions
 {
     public static class ListExtensions
     {
