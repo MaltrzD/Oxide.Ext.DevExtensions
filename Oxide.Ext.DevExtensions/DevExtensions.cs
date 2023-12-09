4dcb6e8c764df9f709393f123d749d303785202b
MaltrzD <97359122+MaltrzD@users.noreply.github.com>
2023-12-10 01:19:44 +0300

    Update DevExtensions.cs
    ошибочка

--- Oxide.Ext.DevExtensions/DevExtensions.cs
+++ Oxide.Ext.DevExtensions/DevExtensions.cs
@@ -5,8 +5,6 @@
     {
         public static string GetMeme()
         {
-            Random rnd = new Random();
-
             string[] memes = new string[] { "карбон крут", "https://youtu.be/dQw4w9WgXcQ?si=ZkgNSrX8D5bR5LeV", "meme" };
 
             return memes.GetRandom();
