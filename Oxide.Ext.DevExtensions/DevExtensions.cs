c3b814d4ec323e22dd900d7033500956e1b6bcb5
MaltrzD <97359122+MaltrzD@users.noreply.github.com>
2023-12-10 01:18:16 +0300

    Initial

---
+++ Oxide.Ext.DevExtensions/DevExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Oxide.Ext.DevExtensions
+{
+
+    public static class DevExtensions
+    {
+        public static string GetMeme()
+        {
+            Random rnd = new Random();
+
+            string[] memes = new string[] { "карбон крут", "https://youtu.be/dQw4w9WgXcQ?si=ZkgNSrX8D5bR5LeV", "meme" };
+
+            return memes.GetRandom();
+        }
+    }
+}
---
+++ Oxide.Ext.DevExtensions/ListExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oxide.Ext.DevExtensions
+{
+    public static class ListExtensions
+    {
+        public static string ToSentenceAdv<T>(this List<T> list)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Capacity = list.Count;
+
+            sb.Append("[");
+
+            foreach (var item in list)
+                sb.Append($"{item}|");
+            sb.Remove(sb.Length - 1, 1);
+
+            sb.Append("]");
+
+
+            return sb.ToString();
+        }
+    }
+}
